036890f0fde8e79c3604797194189e8005508e14
daomoer <643934580@qq.com>
2018-08-11 20:19:10 +0800

    updata
    updata

---
+++ XMLYFM/Const.swift
@@ -0,0 +1,21 @@
+//
+//  Const.swift
+//  XMLYFM
+//
+//  Created by Domo on 2018/7/31.
+//  Copyright © 2018年 知言网络. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Kingfisher
+import SnapKit
+import SwiftyJSON
+import HandyJSON
+
+let YYScreenWidth = UIScreen.main.bounds.size.width
+let YYScreenHeigth = UIScreen.main.bounds.size.height
+let DominantColor = UIColor.init(red: 239/255.0, green: 80/255.0, blue: 88/255.0, alpha: 1)
+let FooterViewColor = UIColor.init(red: 245/255.0, green: 245/255.0, blue: 245/255.0, alpha: 1)
+
+
