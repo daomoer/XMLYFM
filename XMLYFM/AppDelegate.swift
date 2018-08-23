5e882df3e7bc65c50710ed81f72dcc3438cc3864
daomoer <643934580@qq.com>
2018-08-23 19:08:40 +0800

    一些图片资源
    一些图片资源

--- XMLYFM/AppDelegate.swift
+++ XMLYFM/AppDelegate.swift
@@ -66,14 +66,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             [weak tabBarController] tabbarController, viewController, index in
             
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                let alertController = UIAlertController.init(title: nil, message: nil, preferredStyle: .actionSheet)
-                let takePhotoAction = UIAlertAction(title: "Take a photo", style: .default, handler: nil)
-                alertController.addAction(takePhotoAction)
-                let selectFromAlbumAction = UIAlertAction(title: "Select from album", style: .default, handler: nil)
-                alertController.addAction(selectFromAlbumAction)
-                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-                alertController.addAction(cancelAction)
-                tabBarController?.present(alertController, animated: true, completion: nil)
+//                let alertController = UIAlertController.init(title: nil, message: nil, preferredStyle: .actionSheet)
+//                let takePhotoAction = UIAlertAction(title: "Take a photo", style: .default, handler: nil)
+//                alertController.addAction(takePhotoAction)
+//                let selectFromAlbumAction = UIAlertAction(title: "Select from album", style: .default, handler: nil)
+//                alertController.addAction(selectFromAlbumAction)
+//                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+//                alertController.addAction(cancelAction)
+//                tabBarController?.present(alertController, animated: true, completion: nil)
+                let vc = FMPlayController()
+                tabBarController?.present(vc, animated: true, completion: nil)
             }
         }
         
